fc8f8244cf16512e16e9880ce0edbe9a94f3c8c4
P. V. C. L. Jayashnatha <2023cs070@stu.ucsc.cmb.ac.lk>
2026-02-14 21:03:34 +0530

    feat: Update accommodation detail view with SVG icon and enhance CSS styles

--- app/views/traveller/accommodationdetail.view.php
+++ app/views/traveller/accommodationdetail.view.php
@@ -51,7 +51,11 @@
           </div>
 
           <div class="hotel-location">
-            <span class="location-icon">📍</span>
+            <span class="location-icon">
+              <svg width="28" height="28" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" fill="#1abc5b"/>
+              </svg>
+            </span>
             <span id="hotelLocation">Loading...</span>
             <button class="map-btn" onclick="showMap()">View on Map</button>
           </div>
